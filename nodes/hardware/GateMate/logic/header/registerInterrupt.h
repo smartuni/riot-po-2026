7d2c672ff6cf238f3d1684b1e228399c0d08a61c
makrej <makrej.git@proton.me>
2025-05-26 15:43:39 +0200

    Add basic files for logic

---
+++ nodes/hardware/GateMate/logic/header/registerInterrupt.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+#include "board.h"
+#include "ztimer.h"
+#include "periph/gpio.h
+
---
+++ nodes/hardware/GateMate/logic/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "board.h"
+#include "ztimer.h"
+
+
+int main(void){
+
+    return 0;
+}
