36568d4e80437a3a55e711a9d52d7f58acd2232e
thuyan <nguyen.95@campus.tu-berlin.de>
2025-05-26 16:18:00 +0200

    paths

---
+++ nodes/senseMate/bin/adafruit-feather-nrf52840-sense/core_lib/clist.d
@@ -0,0 +1,17 @@
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/core_lib/clist.o: \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/core/lib/clist.c \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/core/lib/include/clist.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/core/lib/include/list.h
+
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h:
+
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/core/lib/include/clist.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/core/lib/include/list.h:
---
+++ nodes/senseMate/bin/adafruit-feather-nrf52840-sense/nimble_transport/monitor.d
@@ -0,0 +1,8 @@
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/nimble_transport/monitor.o: \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/build/pkg/nimble/nimble/transport/src/monitor.c \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h \
+ /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/build/pkg/nimble/porting/npl/riot/include/syscfg/syscfg.h
+
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h:
+
+/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/build/pkg/nimble/porting/npl/riot/include/syscfg/syscfg.h:
