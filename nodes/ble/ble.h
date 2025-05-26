e898199f31ee460c674679cd15b359bf148b4648
Nico Peter <128388731+NicoLikesDinos@users.noreply.github.com>
2025-05-26 17:31:21 +0200

    ble example

---
+++ nodes/ble/ble.h
@@ -0,0 +1,11 @@
+/**
+ * @brief       This module provides BLE send and receive functionality for the feather-nrf52840-sense board
+ *
+ * @author      
+ */
+#ifndef BLE_H
+#define BLE_H
+
+void ble_example();
+
+#endif // BLE_H
