ab20f1667bb8439f7ba55cb487a7e2e22ef66656
Nico Peter <128388731+NicoLikesDinos@users.noreply.github.com>
2025-05-26 17:14:47 +0200

    new folder structure

--- nodes/senseMate/bin/adafruit-feather-nrf52840-sense/board/bat_voltage.d
+++
@@ -1,5 +0,0 @@
-/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/board/bat_voltage.o: \
- /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/boards/adafruit-feather-nrf52840-sense/bat_voltage.c \
- /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h
-
-/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h:
--- nodes/senseMate/bin/adafruit-feather-nrf52840-sense/board/mtd.d
+++
@@ -1,5 +0,0 @@
-/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/board/mtd.o: \
- /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/RIOT/boards/adafruit-feather-nrf52840-sense/mtd.c \
- /home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h
-
-/home/tin/Desktop/Studium/Projekt/riot-po-2025/nodes/senseMate/bin/adafruit-feather-nrf52840-sense/riotbuild/riotbuild.h:
