03629ad305e1a6f84940266f63fb6821541a4cfe
thuyan <nguyen.95@campus.tu-berlin.de>
2025-06-02 15:36:46 +0200

    Header

--- nodes/tables/tables.c
+++ nodes/tables/tables.c
@@ -1,1 +1,12 @@
-#include "table.h"
+#include "tables.h"
+#include <string.h>
+
+#include "embUnit.h"
+#include "embUnit/embUnit.h"
+#include "fmt.h"
+#include "cbor.h"
+#include "container.h"
+
+int target_state_entry_to_cbor(target_state_entry entry, cbor_buffer buffer) {
+    return 0;
+}
