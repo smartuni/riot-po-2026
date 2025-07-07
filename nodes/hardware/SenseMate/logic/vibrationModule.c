f7b905da74fdc76a048d590d5b05b39e0dc1c5bf
schniszl <pfks96@yahoo.de>
2025-07-07 14:09:58 +0200

    vibrating action going on HOT

---
+++ nodes/hardware/SenseMate/logic/vibrationModule.c
@@ -0,0 +1,24 @@
+#include "vibrationModule.h"
+#include <stdio.h>
+#include "board.h"
+#include "ztimer.h"
+#include "periph/gpio.h"
+
+gpio_t vibration = GPIO_PIN(1, 9);
+gpio_mode_t vibtation_mode = GPIO_OUT;
+
+void init_vibration_module(void) {
+    // Initialize the vibration module
+    gpio_init(vibration, vibtation_mode);
+    gpio_clear(vibration);
+}
+
+void start_vibration(void) {
+    // Start vibration
+    gpio_set(vibration);
+}
+
+void stop_vibration(void) {
+    // Stop vibration
+    gpio_clear(vibration);
+}
