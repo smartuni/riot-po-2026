134c43c6164514e2af34302b2405d7ab894d1f1e
schniszl <pfks96@yahoo.de>
2025-08-06 14:15:00 +0200

    documentation written

--- nodes/firmware/senseMate/include/soundModule.h
+++ nodes/firmware/senseMate/include/soundModule.h
@@ -13,17 +13,40 @@ extern event_t ble_sent_sound_event;
 extern event_t tables_news_sound_event;
 extern event_t close_by_todo_sound_event;
 
-
+/*
+ * Initializes the sound module
+ * This function should be called at the start of the program
+ */
 void init_sound_module(void);
 
+/*
+ * Plays the sound for downlink received
+ * This function will post an event to the sound queue
+ */
 void downlink_reveived_sound(void);
 
+/*
+ * Plays the sound for uplink sent
+ * This function will post an event to the sound queue
+ */
 void uplink_sent_sound(void);
 
+/*
+ * Plays the sound for BLE received
+ * This function will post an event to the sound queue
+ */
 void ble_reveived_sound(void);
 
+/*
+ * Plays the sound for BLE sent
+ * This function will post an event to the sound queue
+ */
 void ble_sent_sound(void);
 
+/*
+ * Plays the startup sound
+ * This function will post an event to the sound queue
+ */
 void startup_sound(void);
 
 #endif // SOUNDMODULE_H
--- nodes/firmware/senseMate/include/vibrationModule.h
+++ nodes/firmware/senseMate/include/vibrationModule.h
@@ -1,10 +1,22 @@
 #ifndef VIBRATION_MODULE_H
 #define VIBRATION_MODULE_H
 
+/*
+ * Initializes the vibration module
+ * This function should be called at the start of the program
+ */
 void init_vibration_module(void);
 
+/*
+ * Starts the vibration
+ * This function will set the GPIO pin to start the vibration
+ */
 void start_vibration(void);
-void stop_vibration(void);
 
+/*
+ * Stops the vibration
+ * This function will clear the GPIO pin to stop the vibration
+ */
+void stop_vibration(void);
 
 #endif // VIBRATION_MODULE_H
