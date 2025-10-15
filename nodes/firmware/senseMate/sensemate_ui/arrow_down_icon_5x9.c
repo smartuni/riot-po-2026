3a20b199032016c06a24dc6dd857f5f5ce73b79d
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2025-10-15 17:38:43 +0200

    nodes/firmware/senseMate/sensemate_ui: add new arrow icons and res files for it

---
+++ nodes/firmware/senseMate/sensemate_ui/arrow_down_icon_5x9.c
@@ -0,0 +1,47 @@
+#ifdef __has_include
+    #if __has_include("lvgl.h")
+        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
+            #define LV_LVGL_H_INCLUDE_SIMPLE
+        #endif
+    #endif
+#endif
+
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
+
+
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
+
+#ifndef LV_ATTRIBUTE_IMG_ARROW_DOWN_ICON_5X9
+#define LV_ATTRIBUTE_IMG_ARROW_DOWN_ICON_5X9
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ARROW_DOWN_ICON_5X9 uint8_t arrow_down_icon_5x9_map[] = {
+  0x00, 0x00, 0x00, 0x00, 	/*Color of index 0*/
+  0xff, 0xff, 0xff, 0xff, 	/*Color of index 1*/
+
+  0x20, 
+  0x20, 
+  0x20, 
+  0x20, 
+  0x20, 
+  0x20, 
+  0xf8, 
+  0x70, 
+  0x20, 
+};
+
+const lv_img_dsc_t arrow_down_icon_5x9 = {
+  .header.cf = LV_IMG_CF_INDEXED_1BIT,
+  .header.always_zero = 0,
+  .header.reserved = 0,
+  .header.w = 5,
+  .header.h = 9,
+  .data_size = 17,
+  .data = arrow_down_icon_5x9_map,
+};
