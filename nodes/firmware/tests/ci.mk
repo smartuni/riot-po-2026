e829bcd07f707dfba91492b48f71aba3419cb7ab
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2025-11-20 14:25:16 +0100

    nodes/firmware/tests: add CI HiL configurations

---
+++ nodes/firmware/tests/ci.mk
@@ -0,0 +1,14 @@
+ifdef CI
+  # on the CI machine there's an external UART instead of the USB interface
+  PORT=/dev/ttyUSB0
+  USEMODULE += stdio_uart
+
+  # override the reset command and the programmer reset (see RIOT's reset Make target)
+  RESET = "true"
+  _PROGRAMMER_RESET = stty -F $(PORT) raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
+#   PREFLASHER = $(_PROGRAMMER_RESET) ;sleep 0.1; $(_PROGRAMMER_RESET)
+  PREFLASHER = board_helper --port $(PORT)
+  PREFFLAGS =
+  PREFLASH_DELAY=5
+  TERMFLAGS += --set-dtr=0
+endif
