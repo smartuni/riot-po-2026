e68a57c66dfae4e447c97b0d722314ba60b70c72
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2025-11-20 14:25:16 +0100

    nodes/firmware/tests: add CI HiL configurations

---
+++ nodes/firmware/tests/test_module_cbor_serialization/tests/01-run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+from testrunner import run
+
+
+def testfunc(child):
+    child.expect("8 Tests 0 Failures 0 Ignored")
+    child.expect("OK")
+
+    print("All tests successful")
+
+
+if __name__ == "__main__":
+    sys.exit(run(testfunc, timeout=1))
