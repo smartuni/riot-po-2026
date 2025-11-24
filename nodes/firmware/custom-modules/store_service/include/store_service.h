d02a70698f9bfabd138851d78ed31c235fafcad7
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2025-11-24 09:14:27 +0100

    nodes/firmware/custom-modules/store_service: adjust STORE_ITERATOR macro to pointer type

--- nodes/firmware/custom-modules/store_service/include/store_service.h
+++ nodes/firmware/custom-modules/store_service/include/store_service.h
@@ -162,7 +162,7 @@ typedef struct {
  * @param service   Pointer to the store service
  */
 #define STORE_ITERATOR(name, service)                                                       \
-        size_t name##_needed_size = (service)->interface.iterator_size((service)->context); \
+        size_t name##_needed_size = (service)->interface->iterator_size((service)->context); \
         size_t name##_count = (name##_needed_size + sizeof(store_service_iterator_t) - 1) / \
                               sizeof(store_service_iterator_t);                             \
         store_service_iterator_t name##_buf[name##_count];                                  \
