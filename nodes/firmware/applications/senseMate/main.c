4b5daabcbc74e58e4c906397a9cd1e48d490781f
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2025-11-27 10:36:19 +0100

    nodes/firmware/applications/senseMate: adjust to new tables/serive api, improve ui offloading, drop dead code

--- nodes/firmware/applications/senseMate/main.c
+++ nodes/firmware/applications/senseMate/main.c
@@ -1,3 +1,4 @@
+#include "od.h"
 #include "ztimer.h"
 #include <stdio.h>
 #include <list.h>
@@ -7,12 +8,22 @@
 #include "include/interrupts.h"
 #include "include/events_creation.h"
 #include "tables.h"
+#include "tables/records.h"
 #include "mate_lorawan.h"
 #include "include/soundModule.h"
 #include "include/vibrationModule.h"
 #include "include/sensemate_ui.h"
 #include "mate_ble.h"
 #include "mtd.h"
+#define LOG_LEVEL   LOG_NONE
+#include "log.h"
+#define _LOGDBG(...) LOG_DEBUG("[main]: " __VA_ARGS__)
+#define _LOGINF(...) LOG_INFO("[main]: " __VA_ARGS__)
+
+static const char *ok(bool condition)
+{
+    return condition ? "[OK]" : "[ERROR]";
+}
 
 #define STORAGE_RAM_MOUNT_PATH "/ram0"
 #define STORAGE_MOUNT_PATH STORAGE_RAM_MOUNT_PATH
@@ -23,74 +34,119 @@ extern mtd_dev_t *storage_setup_get_ram_mtd(void);
 tables_context_t *tables;
 
 char ble_send_stack[2*THREAD_STACKSIZE_DEFAULT];
-char ble_reicv_stack[2*THREAD_STACKSIZE_DEFAULT];
+char ble_reicv_stack[4*THREAD_STACKSIZE_MAIN];
 
 int lorawan_started = -1;
 
+table_iterator_t *_all_gates_iterator;
+table_query_t all_gates_query = {
+    .type = RECORD_GATE_REPORT,
+    .writer_id = NULL,
+    .involved_id = NULL
+};
+
+uint32_t _get_known_gate_count(void)
+{
+    uint32_t gates_cnt = 0;
+    TABLE_ITERATOR(iter, tables);
+    table_query_t query;
+    tables_init_query(&query, RECORD_GATE_REPORT, NULL, NULL);
+
+    int res = tables_iterator_init(tables, &iter, &query);
+    _LOGDBG("%s iter init (%d) %s\n", __func__, res, ok(res == 0));
+    if (res) {
+        return false;
+    }
+
+    table_record_t *record;
+
+    while( tables_iterator_next(tables, &iter, &record, NULL, NULL) == 0) {
+        _LOGDBG("%s iter next (%d) %s\n", __func__, res, ok(res == 0));
+        gates_cnt++;
+    }
+
+    return gates_cnt;
+}
+
 static bool _all_gates_iter(ui_data_element_t *prev)
 {
-    gate_id_t next_id = prev->iter_ctx.idx;
+    if (prev->iter_ctx.ptr == NULL) {
+        int res = tables_iterator_init(tables, _all_gates_iterator, &all_gates_query);
+        _LOGDBG("%s iter init (%d) %s\n", __func__, res, ok(res == 0));
+        if (res) {
+            return false;
+        }
+    }
+
+    table_record_t *record;
+
+    int res = tables_iterator_next(tables, _all_gates_iterator, &record, NULL, NULL);
+    _LOGDBG("%s iter next (%d) %s\n", __func__, res, ok(res == 0));
+    if (res) {
+        prev->iter_ctx.ptr = NULL;
+        return false;
+    }
+
     gate_local_info_entry_t *li = &prev->data.local_gate_info;
 
-    for (gate_id_t i = next_id; i < MAX_GATE_COUNT; i++) {
-        if (tables_get_local_info_entry(i, li) == TABLE_SUCCESS) {
-            /* remember where to continue on the next call */
-            prev->iter_ctx.idx = i + 1;
+    table_record_type_t type;
+    get_record_type(record, &type);
+    if (type == RECORD_GATE_REPORT) {
+        table_gate_report_t *rdata;
+        const node_id_t *writer_id;
+        get_record_writer_id(record, &writer_id);
+        _LOGDBG("writer id:\n");
+        if (LOG_LEVEL >= LOG_DEBUG) {
+            od_hex_dump(writer_id, sizeof(node_id_t), 0);
+        }
+        if (get_gate_report_data(record, &rdata) == 0) {
+            _LOGDBG("%s Gate State: %s\n", __func__, gate_state_tostr(rdata->state));
+            //TODO: void get_record_timestamp(const table_record_t *record, hlc_timestamp_t *timestamp);
+            //li->sensor_timestamp = sensor.timestamp;
+            memcpy(li->gateID, writer_id, sizeof(node_id_t));
+            li->sensor_state = rdata->state;
+            li->sensor_data_present = true;
+            prev->iter_ctx.ptr = _all_gates_iterator;
             return true;
         }
     }
     return false;
 }
 
-static bool _set_gate_seen_state_cb(ui_data_element_t *elem)
+static bool _put_gate_observation_cb(ui_data_element_t *elem)
 {
-    gate_seen_state_entry_t *seen_state = &elem->data.seen_state;
-
-    printf("_set_gate_seen_state_cb: %d %d\n", seen_state->gateID, seen_state->state);
-    /* populate remaining data which is not provided from the UI */
-    seen_state->senseMateID = RIOT_CONFIG_DEVICE_ID;
-    seen_state->timestamp = RIOT_CONFIG_DEVICE_ID;
-
-    gate_timestamp_entry_t te;
-    /* FIXME: if the gate was not seen before (and therefore there is no entry in the timestamp table)
-     *        no seen state entry will be generated.
-     *        In the future at least a local time reference should be added. */
-    int res = get_timestamp_entry(seen_state->gateID, &te);
-    if (res == TABLE_SUCCESS) {
-        seen_state->timestamp = te.timestamp;
-        int sres = set_seen_status_entry(seen_state);
-        if (sres == TABLE_NEW_RECORD || sres == TABLE_UPDATED) {
-            ////TODO: move to new tables API
-            //event_post(EVENT_PRIO_MEDIUM, &send_seen_status_table);
-            return true;
-        }
+    table_gate_observation_t *obs = &elem->data.gate_observation;
+    _LOGDBG("%s: %d %s\n", __func__, obs->gate_id[3], gate_state_tostr(obs->state));
+    int res = tables_put_gate_observation(tables, &obs->gate_id, obs->state);
+    if (res == 0) {
+        /* changed */
+        return true;
+    } else {
+        /* not changed */
+        _LOGINF("%s put observation failed!.\n", __func__);
+        return false;
     }
-    return false;
 }
 
 static ui_data_cbs_t _ui_data_cbs = {
     .all_gates_iter = _all_gates_iter,
-    .set_seen_state = _set_gate_seen_state_cb,
+    .put_gate_observation = _put_gate_observation_cb,
     .jobs_iter = NULL,
 };
 
 int main(void) {
     //ztimer_sleep(ZTIMER_MSEC, 3000);
-
     int res = storage_setup_ram_mtd(STORAGE_MOUNT_PATH);
-    printf("storage_setup_ram_mtd: %d\n", res);
-    mtd_dev_t *mtd = storage_setup_get_ram_mtd();
-    printf("MTD '%s' device properties:\n", STORAGE_MOUNT_PATH);
-    printf("sector_count:     %"PRIu32"   \n", mtd->sector_count);
-    printf("pages_per_sector: %"PRIu32"   \n", mtd->pages_per_sector);
-    printf("page_size:        %"PRIu32"   \n", mtd->page_size);
+    _LOGDBG("storage_setup_ram_mtd: %s\n", ok(res == 0));
 
     res = credential_manager_setup(STORAGE_MOUNT_PATH "/cred");
-    printf("credential_manager_setup: %d\n", res);
+    _LOGDBG("credential_manager_setup: %s\n", ok(res == 0));
 
     res = tables_setup(&tables, STORAGE_MOUNT_PATH "/tables");
-    printf("tables_setup: %d\n", res);
+    _LOGDBG("tables_setup: %s\n", ok(res == 0));
 
+    TABLE_ITERATOR(all_gates_iterator, tables);
+    _all_gates_iterator = &all_gates_iterator;
 
     printf("init menu...\n");
     sensemate_ui_init(&_ui_data_cbs);
@@ -104,10 +160,14 @@ int main(void) {
     printf("Device Type: %d device id: %d\n", RIOT_CONFIG_DEVICE_TYPE,
                                               RIOT_CONFIG_DEVICE_ID);
     init_event();
-    init_tables();
+
+    //for (unsigned i = 0; i < 2; i++) {
+    //    printf("put report %d\n", i);
+    //    res = tables_put_gate_report(tables, GATE_STATE_OPEN);
+    //}
     
     puts("starting ble");
-    if (BLE_SUCCESS == ble_init()){
+    if (BLE_SUCCESS == mate_ble_init(tables)){
         puts("Ble init complete");
     } else {
         puts("BLE not started");
@@ -156,8 +216,18 @@ int main(void) {
     }
 
     puts("entering main loop");
+    uint32_t prev_cnt = 0;
     while (1)
     {
+        ui_state->visible_gate_cnt = _get_known_gate_count();
+        //TODO: Re-add below functionality, but now based on new API
+        //ui_state->pending_jobs_cnt = tables_get_jobs_entry_count();
+        //ui_state->visible_mate_cnt = tables_get_closeby_mate_seen_state_entry_count(-80);
+        if (prev_cnt != ui_state->visible_gate_cnt) {
+            sensemate_ui_update();
+            prev_cnt = ui_state->visible_gate_cnt;
+        }
+
         if(lorawan_started == -1){
             lorawan_started = mate_lorawan_start(tables);
             if (lorawan_started == 0) {
@@ -166,175 +236,9 @@ int main(void) {
             }
         }
         ztimer_sleep(ZTIMER_MSEC, 1000);
-        //refresh_display();
-        increment_device_timestamp();
     }
     
     printf("Display demo finished.\n");
 
     return 0;
-}
-
-void fill_tables_test(void){
-    gate_sensor_state_entry_t test;
-    test.gateID = 1;
-    test.state = 1;
-    test.timestamp = 20000;
-    int test_merge = set_is_state_entry(&test);
-    test.gateID = 2;
-    test.state = 1;
-    test.timestamp = 20000;
-    test_merge = set_is_state_entry(&test);
-    test.gateID = 3;
-    test.state = 1;
-    test.timestamp = 20000;
-    test_merge = set_is_state_entry(&test);
-    test.gateID = 4;
-    test.state = 1;
-    test.timestamp = 20000;
-    test_merge = set_is_state_entry(&test);
-    test.gateID = 5;
-    test.state = 1;
-    test.timestamp = 20000;
-    test_merge = set_is_state_entry(&test);
-    test.gateID = 6;
-    test.state = 1;
-    test.timestamp = 20000;
-    test_merge = set_is_state_entry(&test);
-    test.gateID = 7;
-    test.state = 1;
-    test.timestamp = 20000;
-    test_merge = set_is_state_entry(&test);
-    printf("Test merge: %d\n", test_merge);
-
-    gate_seen_state_entry_t test_seen;
-    test_seen.gateID = 1;
-    test_seen.state = 1;
-    test_seen.timestamp = 2000;
-    test_seen.senseMateID = 3;
-    int test_seen_merge = set_seen_status_entry(&test_seen);
-    test_seen.gateID = 2;
-    test_seen.state = 0;
-    test_seen.timestamp = 25000;
-    test_seen.senseMateID = 5;
-    test_seen_merge = set_seen_status_entry(&test_seen);
-    test_seen.gateID = 3;
-    test_seen.state = 1;
-    test_seen.timestamp = 30000;
-    test_seen.senseMateID = 7;
-    test_seen_merge = set_seen_status_entry(&test_seen);
-    test_seen.gateID = 4;
-    test_seen.state = 0;
-    test_seen.timestamp = 28647;
-    test_seen.senseMateID = 5;
-    test_seen_merge = set_seen_status_entry(&test_seen);
-    test_seen.gateID = 5;
-    test_seen.state = 0;
-    test_seen.timestamp = 1000;
-    test_seen.senseMateID = 3;
-    test_seen_merge = set_seen_status_entry(&test_seen);
-    test_seen.gateID = 6;
-    test_seen.state = 1;
-    test_seen.timestamp = 5000;
-    test_seen.senseMateID = 7;
-    test_seen_merge = set_seen_status_entry(&test_seen);
-    test_seen.gateID = 7;
-    test_seen.state = 1;
-    test_seen.timestamp = 10000;
-    test_seen.senseMateID = 5;
-    test_seen_merge = set_seen_status_entry(&test_seen);
-    printf("Test merge: %d\n", test_seen_merge);
-    
-    gate_target_state_entry_t test_target;
-    test_target.gateID = 1;
-    test_target.state = 1;
-    test_target.timestamp = 20000;
-    int test_target_merge = set_target_state_entry(&test_target);
-    test_target.gateID = 2;
-    test_target.state = 0;
-    test_target.timestamp = 25000;
-    test_target_merge = set_target_state_entry(&test_target);
-    test_target.gateID = 3;
-    test_target.state = 1;
-    test_target.timestamp = 30000;
-    test_target_merge = set_target_state_entry(&test_target);
-    test_target.gateID = 4;
-    test_target.state = 0;
-    test_target.timestamp = 28647;
-    test_target_merge = set_target_state_entry(&test_target);
-    test_target.gateID = 5;
-    test_target.state = 0;
-    test_target.timestamp = 1000;
-    test_target_merge = set_target_state_entry(&test_target);
-    test_target.gateID = 6;
-    test_target.state = 1;
-    test_target.timestamp = 5000;
-    test_target_merge = set_target_state_entry(&test_target);
-    test_target.gateID = 7;
-    test_target.state = 1;
-    test_target.timestamp = 10000;
-    test_target_merge = set_target_state_entry(&test_target);
-    printf("Test merge: %d\n", test_target_merge);
-
-    gate_job_entry_t test_job;
-    test_job.gateID = 1;
-    test_job.done = 1;
-    test_job.priority = 1;
-    int test_job_merge = set_jobs_entry(&test_job);
-    test_job.gateID = 2;
-    test_job.done = 0;
-    test_job.priority = 2;
-    test_job_merge = set_jobs_entry(&test_job);
-    test_job.gateID = 3;
-    test_job.done = 0;
-    test_job.priority = 3;
-    test_job_merge = set_jobs_entry(&test_job);
-    test_job.gateID = 4;
-    test_job.done = 0;
-    test_job.priority = 2;
-    test_job_merge = set_jobs_entry(&test_job);
-    test_job.gateID = 5;
-    test_job.done = 0;
-    test_job.priority = 0;
-    test_job_merge = set_jobs_entry(&test_job);
-    test_job.gateID = 6;
-    test_job.done = 0;
-    test_job.priority = 3;
-    test_job_merge = set_jobs_entry(&test_job);
-    test_job.gateID = 7;
-    test_job.done = 0;
-    test_job.priority = 2;
-    test_job_merge = set_jobs_entry(&test_job);
-    printf("Test merge: %d\n", test_job_merge);
-
-    gate_timestamp_entry_t test_timestamp;
-    test_timestamp.gateID = 1;
-    test_timestamp.timestamp = 55000;
-    test_timestamp.rssi = -50;
-    int test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    test_timestamp.gateID = 2;
-    test_timestamp.timestamp = 60000;
-    test_timestamp.rssi = -100;
-    test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    test_timestamp.gateID = 3;
-    test_timestamp.timestamp = 66000;
-    test_timestamp.rssi = 0;
-    test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    test_timestamp.gateID = 4;
-    test_timestamp.timestamp = 70000;
-    test_timestamp.rssi = 170;
-    test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    test_timestamp.gateID = 5;
-    test_timestamp.timestamp = 8000;
-    test_timestamp.rssi = -20;
-    test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    test_timestamp.gateID = 6;
-    test_timestamp.timestamp = 9000;
-    test_timestamp.rssi = -30;
-    test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    test_timestamp.gateID = 7;
-    test_timestamp.timestamp = 10000;
-    test_timestamp.rssi = 0;
-    test_timestamp_merge = set_timestamp_entry(&test_timestamp);
-    printf("Test merge: %d\n", test_timestamp_merge);
 }
