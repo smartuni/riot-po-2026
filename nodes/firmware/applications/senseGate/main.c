7567744da1e9e1425cc1770c118536ac6a671e0d
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2025-11-27 10:47:23 +0100

    nodes/firmware/applications/senseGate: improve logging, drop tables_old, increase stack sizes, start shell

--- nodes/firmware/applications/senseGate/main.c
+++ nodes/firmware/applications/senseGate/main.c
@@ -12,16 +12,28 @@
 #include "inductive_sensor.h"
 #include "include/gate_observer.h"
 #include "mtd.h"
+#define LOG_LEVEL   LOG_DEBUG
+#include "log.h"
+#define _LOGDBG(...) LOG_DEBUG("[main]: " __VA_ARGS__)
+
+#include "personalization.h"
+#define MAX_SIGNATURE_SIZE 80
+#define MAX_SERIALIZED_RECORD_SIZE 128
+
+static const char *ok(bool condition)
+{
+    return condition ? "[OK]" : "[ERROR]";
+}
 
 #define STORAGE_RAM_MOUNT_PATH "/ram0"
 #define STORAGE_MOUNT_PATH STORAGE_RAM_MOUNT_PATH
 extern int credential_manager_setup(const char *db_path);
 extern int tables_setup(tables_context_t **t, const char *db_path);
 extern int storage_setup_ram_mtd(const char *mount_path);
-extern mtd_dev_t *storage_setup_get_ram_mtd(void);
 tables_context_t *tables;
 
-#define TIME_PERIOD_TABLE_UPDATE 30 // const defines time to update table periodically
+//#define TIME_PERIOD_TABLE_UPDATE 30 // const defines time to update table periodically
+#define TIME_PERIOD_TABLE_UPDATE 2 // const defines time to update table periodically
 
 /* PIN label on feather sense: "A1" */
 #define REED_0_PIN_0 GPIO_PIN(0, 5)
@@ -32,8 +44,22 @@ tables_context_t *tables;
 #define INDUCTIVE_SENSOR_ADC_VREF_MV (3300)
 #define INDUCTIVE_SENSOR_VREF_MV     (11000)
 
-char ble_send_stack[2*THREAD_STACKSIZE_DEFAULT];
-char ble_reicv_stack[2*THREAD_STACKSIZE_DEFAULT];
+char ble_send_stack[4*THREAD_STACKSIZE_DEFAULT];
+char ble_reicv_stack[4*THREAD_STACKSIZE_DEFAULT];
+
+extern event_t *_mateble_send_event;
+extern event_queue_t *_mateble_send_queue;
+
+#include "shell.h"
+char shell_stack[2*THREAD_STACKSIZE_DEFAULT];
+void* shell_thread(void* arg)
+{
+    (void)arg;
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
 
 inductive_sensor_t inductive_sensor;
 
@@ -71,8 +97,9 @@ gate_observer_t observer = {
 
 void gate_observer_state_change_cb(gate_state_t new_state)
 {
+    _LOGDBG("%s new state = %s\n", __func__, gate_state_tostr(new_state));
     int res = tables_put_gate_report(tables, new_state);
-    printf("gate_observer_state_change_cb: %d\n", res);
+    _LOGDBG("%s tables_put_gate_report %s\n", __func__, ok(res == 0));
 }
 
 static void _table_update_cb(tables_context_t *ctx, const table_record_t *record,
@@ -81,31 +108,27 @@ static void _table_update_cb(tables_context_t *ctx, const table_record_t *record
     (void)ctx;
     (void)arg;
     (void)query;
-    printf("\n_table_update_cb\n");
     table_record_type_t type;
     get_record_type(record, &type);
-    printf("record type: %d\n", type);
-
-    const node_id_t *writer_id;
-    get_record_writer_id(record, &writer_id);
-    od_hex_dump(writer_id, NODE_ID_SIZE, 0);
-
-    record_sequence_t sequence;
-    get_record_sequence(record, &sequence);
-    printf("record sequence: %llu\n", sequence);
-    printf("record sequence lower32: %lu\n", (uint32_t)sequence);
-
+    _LOGDBG("%s record type: %s\n", __func__, record_type_tostr(type));
+    if (LOG_LEVEL >= LOG_DEBUG) {
+        const node_id_t *writer_id;
+        get_record_writer_id(record, &writer_id);
+        od_hex_dump(writer_id, NODE_ID_SIZE, 0);
+    }
     if (type == RECORD_GATE_REPORT) {
         table_gate_report_t *report;
         int res = get_gate_report_data(record, &report);
         if (res) {
-            printf("get_gate_report_data failed\n");
+            _LOGDBG("get_gate_report_data failed\n");
         } else {
             gate_state_t state;
             get_gate_report_state(report, &state);
-            printf("gate state: %d\n", state);
+            _LOGDBG("gate state: %s\n", gate_state_tostr(state));
         }
     }
+    //TODO: trigger ble send if there was an update
+    //event_post(_mateble_send_queue, _mateble_send_event);
 }
 
 int main(void){
@@ -113,19 +136,24 @@ int main(void){
     ztimer_sleep(ZTIMER_SEC, 3);
     puts("[main]: starting");
 
+    thread_create(
+        shell_stack,
+        sizeof(shell_stack),
+        THREAD_PRIORITY_MAIN - 2,
+        THREAD_CREATE_STACKTEST,
+        shell_thread,
+        NULL,
+       "shell"
+    );
+
     int res = storage_setup_ram_mtd(STORAGE_MOUNT_PATH);
-    printf("storage_setup_ram_mtd: %d\n", res);
-    mtd_dev_t *mtd = storage_setup_get_ram_mtd();
-    printf("MTD '%s' device properties:\n", STORAGE_MOUNT_PATH);
-    printf("sector_count:     %"PRIu32"   \n", mtd->sector_count);
-    printf("pages_per_sector: %"PRIu32"   \n", mtd->pages_per_sector);
-    printf("page_size:        %"PRIu32"   \n", mtd->page_size);
+    _LOGDBG("storage_setup_ram_mtd: %s\n", ok(res == 0));
 
     res = credential_manager_setup(STORAGE_MOUNT_PATH "/cred");
-    printf("credential_manager_setup: %d\n", res);
+    _LOGDBG("credential_manager_setup: %s\n", ok(res == 0));
 
     res = tables_setup(&tables, STORAGE_MOUNT_PATH "/tables");
-    printf("tables_setup: %d\n", res);
+    _LOGDBG("tables_setup: %s\n", ok(res == 0));
 
     table_memo_t memo;
     table_query_t query;
@@ -134,70 +162,64 @@ int main(void){
     tables_init_query(&query, RECORD_GATE_REPORT, NULL, NULL);
     tables_add_memo(tables, &memo, &query, _table_update_cb, cb_arg);
 
-    int isi_res = inductive_sensor_init(&inductive_sensor,
-                                        INDUCTIVE_SENSOR_DCDC_PWR_PIN,
-                                        INDUCTIVE_SENSOR_DCDC_PWR_PIN_AH,
-                                        INDUCTIVE_SENSOR_ADC_LINE,
-                                        INDUCTIVE_SENSOR_ADC_VREF_MV,
-                                        INDUCTIVE_SENSOR_VREF_MV);
+    res = inductive_sensor_init(&inductive_sensor,
+                                INDUCTIVE_SENSOR_DCDC_PWR_PIN,
+                                INDUCTIVE_SENSOR_DCDC_PWR_PIN_AH,
+                                INDUCTIVE_SENSOR_ADC_LINE,
+                                INDUCTIVE_SENSOR_ADC_VREF_MV,
+                                INDUCTIVE_SENSOR_VREF_MV);
+    _LOGDBG("inductive_sensor_init %s\n", ok(res == ANALOG_GATE_SENSOR_SUCCESS));
 
-    if (isi_res != ANALOG_GATE_SENSOR_SUCCESS) {
-        printf("[main]: inductive sensor init failed! %d\n", isi_res);
-    }
+    res = gate_observer_init(&observer, &observer.config, gate_observer_state_change_cb);
+    _LOGDBG("gate_observer_init %s\n", ok(res == 0));
 
-    int goi_res = gate_observer_init(&observer, &observer.config,
-                                     gate_observer_state_change_cb);
-
-    if (goi_res != 0) {
-        printf("[main]: gate_observer_init failed! %d\n", goi_res);
-    }
-
-    puts("[main]: reading initial door state");
+    _LOGDBG("reading initial door state\n");
     gate_observer_state_t obs_state;
     gate_state_t initial_gate_state = gate_observer_get_state(&observer, &obs_state);
 
     for (int i = 0; i < GATE_OBSERVER_LIMITSWITCH_SENSOR_CNT; i++) {
-        printf("[main]: limit switch[%d]: %s\n", i, obs_state.ls_states[i] == LIMIT_SWITCH_ENGAGED ? "CLOSED" : "OPEN");
+        _LOGDBG("limit switch[%d]: %s\n", i, obs_state.ls_states[i] == LIMIT_SWITCH_ENGAGED ? "CLOSED" : "OPEN");
     }
 
     for (int i = 0; i < GATE_OBSERVER_DISTANCE_SENSOR_CNT; i++) {
         uint32_t distance = obs_state.distances[i];
-        printf("[gate_sensor]: distance[%d]: %lu\n", i, distance);
+        _LOGDBG("distance[%d]: %lu\n", i, distance);
     }
 
-    printf("[main]: initial gate state: %s\n",
+    _LOGDBG("initial gate state: %s\n",
             initial_gate_state == GATE_STATE_CLOSED ? "CLOSED" :
             (initial_gate_state == GATE_STATE_OPEN ? "OPEN" : "INVALID"));
     
-    puts("[main]: put initial state to tables...");
+    _LOGDBG("put initial state to tables...\n");
     res = tables_put_gate_report(tables, initial_gate_state);
-    printf("tables_put_gate_report: %d\n", res);
+    _LOGDBG("put initial gate report %s\n", ok(res == 0));
 
     // start lorawan
-    puts("[main]: starting lorawan");
+    _LOGDBG("starting lorawan...\n");
     int lorawanstarted = mate_lorawan_start(tables);
     if (-1 == lorawanstarted){
-        printf("[main]: starting lorawan failed");
+        _LOGDBG("starting lorawan failed\n");
     }
 
     //start thread init bluetooth
-    puts("[main]: starting ble");
-    if (BLE_SUCCESS == ble_init()){
-        puts("[main]: BLE init complete");
+    _LOGDBG("starting [mate_ble]\n");
+    if (BLE_SUCCESS == mate_ble_init(tables)){
+        _LOGDBG("[mate_ble] init complete\n");
     } else {
-        puts("[main]: BLE not started");
+        _LOGDBG("[mate_ble] not started\n");
     }
 
     thread_create(
         ble_send_stack,
         sizeof(ble_send_stack),
-        THREAD_PRIORITY_MAIN - 2,
+        THREAD_PRIORITY_MAIN - 3,
+        //THREAD_PRIORITY_MAIN - 2,
         THREAD_CREATE_STACKTEST,
         ble_send_loop,
         NULL,
        "bleSend"
     );
-    
+
     thread_create(
         ble_reicv_stack,
         sizeof(ble_reicv_stack),
@@ -209,30 +231,32 @@ int main(void){
     );
 
     int timeToUpdateTable = 0; // var to update table periodically
-    
+    int put_cnt = 0;
+    int put_err_cnt = 0;
     while(1){
 
         if (-1 == lorawanstarted){
             lorawanstarted = mate_lorawan_start(tables);
         }
-        
-        increment_device_timestamp();
+
         ztimer_sleep(ZTIMER_MSEC,1000);
-    
+
         if (timeToUpdateTable == TIME_PERIOD_TABLE_UPDATE) {
-            printf("updating own reported state\n");
+            _LOGDBG("get current observer state...\n");
             gate_state_t current_gate_state = gate_observer_get_state(&observer, &obs_state);
+            _LOGDBG("current state: %s\n", gate_state_tostr(current_gate_state));
+            _LOGDBG("put new reported state...\n");
             res = tables_put_gate_report(tables, current_gate_state);
-            if (res == 0) {
-                printf("tables_put_gate_report: %d\n", res);
+            _LOGDBG("tables_put_gate_report %s\n", ok(res == 0));
+            put_cnt++;
+            if (res) {
+                put_err_cnt++;
             }
             timeToUpdateTable = 0;
         } else {
             timeToUpdateTable++;
         }
-
-        
-
     }
+
     return 0;
 }
