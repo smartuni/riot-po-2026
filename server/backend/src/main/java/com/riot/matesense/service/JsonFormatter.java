b29f2064cd963f030cddc165d764130a70b0bebd
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2025-12-03 13:13:09 +0100

    server/backend/~/service/JsonFormatter: amke new decoder logic easier to read

--- server/backend/src/main/java/com/riot/matesense/service/JsonFormatter.java
+++ server/backend/src/main/java/com/riot/matesense/service/JsonFormatter.java
@@ -71,7 +71,12 @@ public class JsonFormatter {
         int messageType = (int)rawData.get(1);
         RecordType recordType = RecordType.fromCode((int)rawData.get(2));
         byte[] writerId= (byte[])rawData.get(3);
+        int hlc_phy = (int)rawData.get(5);
+        int hlc_log = (int)rawData.get(6);
         //===== Header -----^^^^^
+
+        // TODO: add proper support for HLC instead fo this custom conversion onto the old int-timestamp
+        int timestamp = hlc_phy * 1000 + hlc_log;
         /*
         int cnt = 0;
         for (Object o: rawData) {
@@ -84,15 +89,16 @@ public class JsonFormatter {
         */
 
         if (GATE_REPORT.equals(recordType)) {
-            // id[3] := ID number (without device type),  gateState          ,  physical part of HLC
-            StatusEntry se = new StatusEntry(writerId[3], (int)rawData.get(7), (int)rawData.get(5)); // logical part of HLC: rawData.get(6)
+            int gateid = writerId[3];
+            int gateState = (int)rawData.get(7);
+            StatusEntry se = new StatusEntry(gateid, gateState, timestamp);
             Message message = new Message(IST_STATE.getCode(), List.of(se));
             return jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(message);
         } else if (GATE_OBSERVATION.equals(recordType)) {
             List<SeenTableEntry> seenTableList = new ArrayList<>();
             byte[] gateId= (byte[])rawData.get(7);
+            //TODO: add native support of new node_id type
             int gateid = gateId[3];
-            int timestamp = (int)rawData.get(5);
             int gateState = (int)rawData.get(8);
             int mateid = writerId[3];
 
