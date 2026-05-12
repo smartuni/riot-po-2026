d6c9f95c3f533dc79652ce39fffcd9b56df0e6a5
TheUnlimited64 <Theunlimited64@gmail.com>
2026-05-12 14:25:41 +0200

    feat(store): wire WS middleware and dispatch APP_START

--- server/frontend/src/app/App.jsx
+++ server/frontend/src/app/App.jsx
@@ -1,7 +1,9 @@
 import '../shared/styles/App.css';
-import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './store';
+import { APP_START, APP_STOP } from './store/middleware/wsMiddleware';
 import LandingPage from '../pages/LandingPage';
 import LoginPage from '../pages/LoginPage';
 import DashboardPage from '../pages/DashboardPage';
@@ -10,10 +12,18 @@ import UserPage from '../pages/UserPage';
 import DashboardViewPage from '../pages/DashboardViewPage';
 import DashboardGuestPage from '../pages/DashboardGuestPage';
 
-function App() {
+function AppContent() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: APP_START });
+    return () => {
+      dispatch({ type: APP_STOP });
+    };
+  }, [dispatch]);
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
@@ -24,6 +34,13 @@ function App() {
         <Route path='/dashboard-guest' element={<DashboardGuestPage />}></Route>
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AppContent />
     </Provider>
   );
 }
