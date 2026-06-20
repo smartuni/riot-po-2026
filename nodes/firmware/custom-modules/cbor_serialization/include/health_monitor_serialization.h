408f7ca9dcaecff5cd8942f6741fcb65a73bca66
Maverick <maverick.widjaja@haw-hamburg.de>
2026-06-20 14:24:51 +0200

    add copyright, combined both #pragma once and #IFNDEF

--- nodes/firmware/custom-modules/cbor_serialization/include/health_monitor_serialization.h
+++ nodes/firmware/custom-modules/cbor_serialization/include/health_monitor_serialization.h
@@ -1,4 +1,19 @@
+/*
+ * Copyright (C) 2025 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @brief       Health Monitor Serialization for senseGate
+ * @author      Maverick Widjaja <Maverick.widjaja@haw-hamburg.de>
+ */
+
 #pragma once
+#ifndef HEALTH_MONITOR_SERIALIZATION_H
+#define HEALTH_MONITOR_SERIALIZATION_H
 
 #include "health_monitor_payload.h"
 #include "cbor_serialization.h"
@@ -6,8 +21,6 @@
 #include <stdint.h>
 
 #define MESSAGE_TYPE_HEALTH_MONITOR 0x04
-
-
 
 /**
  * @brief Same as cbor_serialize_record but leave out signature field
@@ -21,3 +34,5 @@
  * @retval negative value on error
  */
 int health_monitor_serialize_record_no_sig(const health_monitor_payload_t* record, uint8_t* out, size_t* out_len, bool use_array);
+
+#endif
--- nodes/firmware/custom-modules/health_monitor/include/health_monitor_payload.h
+++ nodes/firmware/custom-modules/health_monitor/include/health_monitor_payload.h
@@ -1,4 +1,19 @@
+/*
+ * Copyright (C) 2025 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @brief       Payload structure for the health monitoring for SenseGate
+ * @author      Maverick Widjaja <Maverick.widjaja@haw-hamburg.de>
+ */
 #pragma once
+#ifndef BATTERY_VOLTAGE_MONITOR_H
+#define BATTERY_VOLTAGE_MONITOR_H
+
 #include <stdint.h>
 
 #define HEALTH_MONITOR_PAYLOAD_SIZE 4
@@ -16,13 +31,14 @@ enum {
 	DISCHARGING_LOW_BATTERY
 };
 
-typedef struct{
+typedef struct {
 	battery_status_t battery_status;
-	int16_t voltage_mv; 
+	int16_t voltage_mv;
 } battery_info_t; //1+2 bytes
-
 
 typedef struct {
 	shock_status_t shock_status; // 1 byte
 	battery_info_t battery_info; // 3 bytes
 } health_monitor_payload_t; // total 4 bytes
+
+#endif
