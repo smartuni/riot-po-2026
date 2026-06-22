83014d267863bae6448f4cd3e843865ab64198f5
Maverick <maverick.widjaja@haw-hamburg.de>
2026-06-22 14:06:29 +0200

    add update period and multi threading

--- nodes/firmware/custom-modules/health_monitor/health_monitor.c
+++ nodes/firmware/custom-modules/health_monitor/health_monitor.c
@@ -1,13 +1,14 @@
 #include "health_monitor.h"
 
-health_monitor_t* health_monitor_new(void) {
+health_monitor_t* health_monitor_new(int update_period_sec) {
 	health_monitor_t* monitor = malloc(sizeof(health_monitor_t));
+	monitor->update_period_sec = update_period_sec;
 
-    int threshold_mv = 3000; // TODO adjust this later
-    //init battery monitoring
-	monitor->battery_monitor = battery_voltage_monitor_new(threshold_mv); // TODO adjust threshold later
+	//init battery monitoring
+	int threshold_mv = 3000; // TODO adjust this later
+	monitor->battery_monitor = battery_voltage_monitor_new(threshold_mv);
 
-    //init shock detection
+	//init shock detection
 	int shock_threshold = 15000; // TODO adjust this later
 	int sample_size = 2048; // TODO adjust this later
 	int sampling_period_ms = 1;
@@ -16,29 +17,45 @@ health_monitor_t* health_monitor_new(void) {
 	return monitor;
 }
 
-int health_monitor_start(health_monitor_t* monitor) {
-    //start battery monitoring
-	// if (monitor->battery_monitor) {
-	// 	battery_voltage_monitor_start(monitor->battery_monitor);
-	// } else {
-	// 	return -1;
-	// }
+static void* thread_function(void* monitor_void) {
+	health_monitor_t* monitor = (health_monitor_t*)monitor_void;
+	while (monitor->running) {
+		//get battery info
+		//get shock status
+		ztimer_sleep(ZTIMER_SEC, monitor->update_period_sec);
+	}
+	return NULL;
+}
 
-    //start shock detection
+int health_monitor_start(health_monitor_t* monitor) {
+	if (monitor == NULL) {
+		return -1;
+	}
+	//start shock detection
 	if (monitor->shock_detector) {
 		shock_detector_start(monitor->shock_detector);
 	} else {
 		return -1;
 	}
 
+	monitor->thread_pid = thread_create(monitor->thread_stack,
+										sizeof(monitor->thread_stack),
+										THREAD_PRIORITY_MAIN - 1,
+										THREAD_CREATE_STACKTEST,
+										thread_function,
+										(void*)monitor, "Health Monitor Thread");
+
 	return 0;
 }
 
-int health_monitor_stop(health_monitor_t* monitor) {
+int health_monitor_delete(health_monitor_t* monitor) {
 	if (monitor == NULL) {
-
 		return -1;
 	}
-	printf(" health monitor is stopped.\n");
+	shock_detector_delete(monitor->shock_detector);
+	free(monitor->shock_detector);
+	battery_voltage_monitor_delete(monitor->battery_monitor);
+	free(monitor->battery_monitor);
+	free(monitor);
 	return 0;
 }
