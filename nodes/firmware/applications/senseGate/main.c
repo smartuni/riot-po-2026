c5b40a797ce6004532bd9b7e4f71867422f548d0
Maverick <maverick.widjaja@haw-hamburg.de>
2026-06-26 09:51:57 +0200

    Revert "revert later"
    This reverts commit f690c1ed5982c05a414ac5da702c28031de89bd1.

--- nodes/firmware/applications/senseGate/main.c
+++ nodes/firmware/applications/senseGate/main.c
@@ -23,8 +23,9 @@
 #define MAX_SIGNATURE_SIZE 80
 #define MAX_SERIALIZED_RECORD_SIZE 128
 
-static const char* ok(bool condition) {
-	return condition ? "[OK]" : "[ERROR]";
+static const char *ok(bool condition)
+{
+    return condition ? "[OK]" : "[ERROR]";
 }
 
 #define STORAGE_RAM_MOUNT_PATH "/ram0"
@@ -36,10 +37,10 @@ static const char* ok(bool condition) {
 #define STORAGE_MOUNT_PATH STORAGE_RAM_MOUNT_PATH
 #endif
 
-extern int credential_manager_setup(const char* db_path);
-extern int tables_setup(tables_context_t** t, const char* db_path);
-extern int storage_setup_ram_mtd(const char* mount_path);
-tables_context_t* tables;
+extern int credential_manager_setup(const char *db_path);
+extern int tables_setup(tables_context_t **t, const char *db_path);
+extern int storage_setup_ram_mtd(const char *mount_path);
+tables_context_t *tables;
 
 #define TIME_PERIOD_TABLE_UPDATE 30 // const defines time to update table periodically
 //#define TIME_PERIOD_TABLE_UPDATE 2 // const defines time to update table periodically
@@ -47,90 +48,98 @@ tables_context_t* tables;
 /* PIN label on feather sense: "A1" */
 #define REED_0_PIN_0 GPIO_PIN(0, 5)
 
-#define INDUCTIVE_SENSOR_DCDC_PWR_PIN GPIO_PIN(0, 4)
+#define INDUCTIVE_SENSOR_DCDC_PWR_PIN GPIO_PIN(0,4)
 #define INDUCTIVE_SENSOR_DCDC_PWR_PIN_AH (false)
 #define INDUCTIVE_SENSOR_ADC_LINE (4)
 #define INDUCTIVE_SENSOR_ADC_VREF_MV (3300)
-#define INDUCTIVE_SENSOR_VREF_MV (11000)
+#define INDUCTIVE_SENSOR_VREF_MV     (11000)
 
 static kernel_pid_t ble_tx_pid = KERNEL_PID_UNDEF;
 
 #include "shell.h"
-char shell_stack[2 * THREAD_STACKSIZE_DEFAULT];
-void* shell_thread(void* arg) {
-	(void)arg;
-	char line_buf[SHELL_DEFAULT_BUFSIZE];
-	shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+char shell_stack[2*THREAD_STACKSIZE_DEFAULT];
+void* shell_thread(void* arg)
+{
+    (void)arg;
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
-	return 0;
+    return 0;
 }
 
 inductive_sensor_t inductive_sensor;
 
-uint32_t inductive_sensor_measure_cb(void* ctx) {
-	inductive_sensor_t* is = (inductive_sensor_t*)ctx;
-	inductive_sensor_power(is, true);
-	ztimer_sleep(ZTIMER_MSEC, 50);
-	uint32_t sum = 0;
-	unsigned nsamples = 16;
-	for (unsigned i = 0; i < nsamples; i++) {
-		sum += inductive_sensor_sample(&inductive_sensor);
-	}
-	inductive_sensor_power(is, false);
+uint32_t inductive_sensor_measure_cb(void *ctx)
+{
+    inductive_sensor_t *is = (inductive_sensor_t*)ctx;
+    inductive_sensor_power(is, true);
+    ztimer_sleep(ZTIMER_MSEC, 50);
+    uint32_t sum = 0;
+    unsigned nsamples = 16;
+    for (unsigned i = 0; i < nsamples; i++) {
+       sum += inductive_sensor_sample(&inductive_sensor);
+    }
+    inductive_sensor_power(is, false);
 
-	uint32_t avg = sum / nsamples;
-	return inductive_sensor_sample2adc_voltage(is, avg);
+    uint32_t avg = sum / nsamples;
+    return inductive_sensor_sample2adc_voltage(is, avg);
 }
 
 gate_observer_t observer = {
-	.config = {
+    .config = {
 #if RIOT_CONFIG_DEVICE_ID == 1
-			   .distance_sensor_confs = {
-			{.closed_min = 30,
-			 .closed_max = 2000,
-			 .measure_cb_ctx = &inductive_sensor,
-			 .measure_distance_cb = inductive_sensor_measure_cb},
-		},
+        .distance_sensor_confs = {
+                                   { .closed_min = 30,
+                                     .closed_max = 2000,
+                                     .measure_cb_ctx = &inductive_sensor,
+                                     .measure_distance_cb = inductive_sensor_measure_cb },
+                                 },
 #endif
-			   .limit_switch_confs = {{.pin = REED_0_PIN_0, .pull_conf = GPIO_IN_PU, .closed_level = false}},
-			   },
+        .limit_switch_confs = {
+                                { .pin = REED_0_PIN_0,
+                                  .pull_conf = GPIO_IN_PU,
+                                  .closed_level = false }
+                              },
+    },
 };
 
-void gate_observer_state_change_cb(gate_state_t new_state) {
-	_LOGDBG("%s new state = %s\n", __func__, gate_state_tostr(new_state));
-	int res = tables_put_gate_report(tables, new_state);
-	_LOGDBG("%s tables_put_gate_report %s\n", __func__, ok(res == 0));
-	// if ble-mate is ready to receive messages
-	if (ble_tx_pid != KERNEL_PID_UNDEF) {
-		table_query_t* q = malloc(sizeof(table_query_t));
-		if (q) {
-			tables_init_query(q, RECORD_GATE_REPORT, &self_node_id, NULL);
-			msg_t m = {.type = MATE_BLE_TX_QUERY_MATCHES_MSG_TYPE,
-					   .content.ptr = q};
-			msg_send(&m, ble_tx_pid);
-		}
-	}
+void gate_observer_state_change_cb(gate_state_t new_state)
+{
+    _LOGDBG("%s new state = %s\n", __func__, gate_state_tostr(new_state));
+    int res = tables_put_gate_report(tables, new_state);
+    _LOGDBG("%s tables_put_gate_report %s\n", __func__, ok(res == 0));
+    // if ble-mate is ready to receive messages
+    if (ble_tx_pid != KERNEL_PID_UNDEF) {
+        table_query_t *q = malloc(sizeof(table_query_t));
+        if (q) {
+            tables_init_query(q, RECORD_GATE_REPORT, &self_node_id, NULL);
+            msg_t m = { .type = MATE_BLE_TX_QUERY_MATCHES_MSG_TYPE,
+                        .content.ptr = q };
+            msg_send(&m, ble_tx_pid);
+        }
+    }
 }
 
-static void _table_update_cb(tables_context_t* ctx, const table_record_t* record,
-							 const table_query_t* query, void* arg) {
-	(void)ctx;
-	(void)arg;
-	(void)query;
-	if (LOG_LEVEL >= LOG_DEBUG) {
-		char record_str[TABLE_RECORD_STRING_SIZE];
-		record_tostr(record, record_str, sizeof(record_str));
-		_LOGDBG("UPDT: %s\n", record_str);
-	}
+static void _table_update_cb(tables_context_t *ctx, const table_record_t *record,
+                             const table_query_t *query, void *arg)
+{
+    (void)ctx;
+    (void)arg;
+    (void)query;
+    if (LOG_LEVEL >= LOG_DEBUG) {
+        char record_str[TABLE_RECORD_STRING_SIZE];
+        record_tostr(record, record_str, sizeof(record_str));
+        _LOGDBG("UPDT: %s\n", record_str);
+    }
 
-	// don't unconditionally trigger TX here.
-	// The change may aswell come from a recent RX.
+    // don't unconditionally trigger TX here.
+    // The change may aswell come from a recent RX.
 }
 
 int main(void) {
-	/* Sleep so that we do not miss this message while connecting */
-	ztimer_sleep(ZTIMER_SEC, 10);
-	puts("[main]: starting");
+    /* Sleep so that we do not miss this message while connecting */
+    ztimer_sleep(ZTIMER_SEC, 10);
+    puts("[main]: starting");
 
 	thread_create(
 		shell_stack,
@@ -142,126 +151,101 @@ int main(void) {
 		"shell"
 	);
 
-	int res = storage_setup_ram_mtd(STORAGE_MOUNT_PATH);
-	_LOGDBG("storage_setup_ram_mtd: %s\n", ok(res == 0));
+    int res = storage_setup_ram_mtd(STORAGE_MOUNT_PATH);
+    _LOGDBG("storage_setup_ram_mtd: %s\n", ok(res == 0));
 
-	res = credential_manager_setup(STORAGE_MOUNT_PATH "/cred");
-	_LOGDBG("credential_manager_setup: %s\n", ok(res == 0));
+    res = credential_manager_setup(STORAGE_MOUNT_PATH "/cred");
+    _LOGDBG("credential_manager_setup: %s\n", ok(res == 0));
 
-	res = tables_setup(&tables, STORAGE_MOUNT_PATH "/tables");
-	_LOGDBG("tables_setup: %s\n", ok(res == 0));
+    res = tables_setup(&tables, STORAGE_MOUNT_PATH "/tables");
+    _LOGDBG("tables_setup: %s\n", ok(res == 0));
 
-	table_memo_t memo;
-	table_query_t query;
+    table_memo_t memo;
+    table_query_t query;
 
-	void* cb_arg = 0;
-	tables_init_query(&query, RECORD_GATE_REPORT, NULL, NULL);
-	tables_add_memo(tables, &memo, &query, _table_update_cb, cb_arg);
+    void *cb_arg = 0;
+    tables_init_query(&query, RECORD_GATE_REPORT, NULL, NULL);
+    tables_add_memo(tables, &memo, &query, _table_update_cb, cb_arg);
 
 #if RIOT_CONFIG_DEVICE_ID == 1
-	res = inductive_sensor_init(&inductive_sensor,
-								INDUCTIVE_SENSOR_DCDC_PWR_PIN,
-								INDUCTIVE_SENSOR_DCDC_PWR_PIN_AH,
-								INDUCTIVE_SENSOR_ADC_LINE,
-								INDUCTIVE_SENSOR_ADC_VREF_MV,
-								INDUCTIVE_SENSOR_VREF_MV);
-	_LOGDBG("inductive_sensor_init %s\n", ok(res == ANALOG_GATE_SENSOR_SUCCESS));
+    res = inductive_sensor_init(&inductive_sensor,
+                                INDUCTIVE_SENSOR_DCDC_PWR_PIN,
+                                INDUCTIVE_SENSOR_DCDC_PWR_PIN_AH,
+                                INDUCTIVE_SENSOR_ADC_LINE,
+                                INDUCTIVE_SENSOR_ADC_VREF_MV,
+                                INDUCTIVE_SENSOR_VREF_MV);
+    _LOGDBG("inductive_sensor_init %s\n", ok(res == ANALOG_GATE_SENSOR_SUCCESS));
 #endif
 
-	res = gate_observer_init(&observer, &observer.config, gate_observer_state_change_cb);
-	_LOGDBG("gate_observer_init %s\n", ok(res == 0));
+    res = gate_observer_init(&observer, &observer.config, gate_observer_state_change_cb);
+    _LOGDBG("gate_observer_init %s\n", ok(res == 0));
 
-	_LOGDBG("reading initial door state\n");
-	gate_observer_state_t obs_state;
-	gate_state_t initial_gate_state = gate_observer_get_state(&observer, &obs_state);
+    _LOGDBG("reading initial door state\n");
+    gate_observer_state_t obs_state;
+    gate_state_t initial_gate_state = gate_observer_get_state(&observer, &obs_state);
 
-	for (int i = 0; i < GATE_OBSERVER_LIMITSWITCH_SENSOR_CNT; i++) {
-		_LOGDBG("limit switch[%d]: %s\n", i, obs_state.ls_states[i] == LIMIT_SWITCH_ENGAGED ? "CLOSED" : "OPEN");
-	}
+    for (int i = 0; i < GATE_OBSERVER_LIMITSWITCH_SENSOR_CNT; i++) {
+        _LOGDBG("limit switch[%d]: %s\n", i, obs_state.ls_states[i] == LIMIT_SWITCH_ENGAGED ? "CLOSED" : "OPEN");
+    }
 
-	for (int i = 0; i < GATE_OBSERVER_DISTANCE_SENSOR_CNT; i++) {
-		uint32_t distance = obs_state.distances[i];
-		_LOGDBG("distance[%d]: %lu\n", i, distance);
-	}
+    for (int i = 0; i < GATE_OBSERVER_DISTANCE_SENSOR_CNT; i++) {
+        uint32_t distance = obs_state.distances[i];
+        _LOGDBG("distance[%d]: %lu\n", i, distance);
+    }
 
-	_LOGDBG("initial gate state: %s\n",
-			initial_gate_state == GATE_STATE_CLOSED ? "CLOSED" : (initial_gate_state == GATE_STATE_OPEN ? "OPEN" : "INVALID"));
+    _LOGDBG("initial gate state: %s\n",
+            initial_gate_state == GATE_STATE_CLOSED ? "CLOSED" :
+            (initial_gate_state == GATE_STATE_OPEN ? "OPEN" : "INVALID"));
 
-	_LOGDBG("put initial state to tables...\n");
-	res = tables_put_gate_report(tables, initial_gate_state);
-	_LOGDBG("put initial gate report %s\n", ok(res == 0));
+    _LOGDBG("put initial state to tables...\n");
+    res = tables_put_gate_report(tables, initial_gate_state);
+    _LOGDBG("put initial gate report %s\n", ok(res == 0));
 
-	// start lorawan
-	_LOGDBG("starting lorawan...\n");
-	int lorawanstarted = mate_lorawan_start(tables);
-	if (-1 == lorawanstarted) {
-		_LOGDBG("starting lorawan failed\n");
-	}
+    // start lorawan
+    _LOGDBG("starting lorawan...\n");
+    int lorawanstarted = mate_lorawan_start(tables);
+    if (-1 == lorawanstarted){
+        _LOGDBG("starting lorawan failed\n");
+    }
 
-	//start thread init bluetooth
-	_LOGDBG("starting [mate_ble]\n");
-	if (BLE_SUCCESS == mate_ble_init(tables, &ble_tx_pid)) {
-		_LOGDBG("[mate_ble] init complete\n");
-	} else {
-		_LOGDBG("[mate_ble] not started\n");
-	}
+    //start thread init bluetooth
+    _LOGDBG("starting [mate_ble]\n");
+    if (BLE_SUCCESS == mate_ble_init(tables, &ble_tx_pid)){
+        _LOGDBG("[mate_ble] init complete\n");
+    } else {
+        _LOGDBG("[mate_ble] not started\n");
+    }
 
-	int timeToUpdateTable = 0; // var to update table periodically
-	int put_cnt = 0;
-	int put_err_cnt = 0;
+    int timeToUpdateTable = 0; // var to update table periodically
+    int put_cnt = 0;
+    int put_err_cnt = 0;
 
-	// health_monitor_t* monitor = health_monitor_new(10);
-	// if (monitor) {
-	// 	health_monitor_start(monitor);
-	// } else {
-	// 	_LOGDBG("Failed to initialize health monitor\n");
-	// }
+   health_monitor_t* monitor = health_monitor_new(10);
+   if (monitor) {
+       health_monitor_start(monitor);
+   } else {
+       _LOGDBG("Failed to initialize health monitor\n");
+   }
 
-	//init the payload
-	health_monitor_payload_t payload;
-	while (1) {
-		ztimer_sleep(ZTIMER_MSEC, 1000);
+    while(1){
+        ztimer_sleep(ZTIMER_MSEC,1000);
 
-		if (timeToUpdateTable == TIME_PERIOD_TABLE_UPDATE) {
-			_LOGDBG("get current observer state...\n");
-			gate_state_t current_gate_state = gate_observer_get_state(&observer, &obs_state);
-			_LOGDBG("current state: %s\n", gate_state_tostr(current_gate_state));
-			_LOGDBG("put new reported state...\n");
-			res = tables_put_gate_report(tables, current_gate_state);
-			_LOGDBG("tables_put_gate_report %s\n", ok(res == 0));
-			put_cnt++;
-			if (res) {
-				put_err_cnt++;
-			}
-			timeToUpdateTable = 0;
-		} else {
-			timeToUpdateTable++;
-		}
+        if (timeToUpdateTable == TIME_PERIOD_TABLE_UPDATE) {
+            _LOGDBG("get current observer state...\n");
+            gate_state_t current_gate_state = gate_observer_get_state(&observer, &obs_state);
+            _LOGDBG("current state: %s\n", gate_state_tostr(current_gate_state));
+            _LOGDBG("put new reported state...\n");
+            res = tables_put_gate_report(tables, current_gate_state);
+            _LOGDBG("tables_put_gate_report %s\n", ok(res == 0));
+            put_cnt++;
+            if (res) {
+                put_err_cnt++;
+            }
+            timeToUpdateTable = 0;
+        } else {
+            timeToUpdateTable++;
+        }
+    }
 
-		//get battery info
-		payload.battery_info.battery_status = CHARGING;
-		payload.battery_info.voltage_mv = 3700;
-		//get shock status
-		payload.shock_status = NO_SHOCK;
-
-		//serialize the payload
-		uint8_t buffer[64];
-		size_t buff_size = sizeof(buffer);
-		health_monitor_serialize_record_no_sig(&payload, buffer, &buff_size, true);
-
-		LOG_DEBUG("[main.c:%d] Serialized health monitor payload, size: %d bytes:\n", __LINE__, buff_size);
-		for (size_t i = 0; i < buff_size; i++) {
-			printf("%02X ", buffer[i]);
-		}
-		LOG_DEBUG("\n");
-		//send the payload via lorawan
-		int status = send_lorawan_packet(buffer, buff_size);
-		if (status == 0) {
-			LOG_DEBUG("[main.c:%d] Sent health monitor payload via LoRaWAN\n", __LINE__);
-		} else {
-			LOG_DEBUG("[main.c:%d] Failed to send health monitor payload via LoRaWAN: lorawan is not initialized yet\n", __LINE__);
-		}
-	}
-
-	return 0;
+    return 0;
 }
