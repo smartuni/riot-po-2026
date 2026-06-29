4a67a4232aefbda735d8efaddb63c180783ffbe5
Maverick <maverick.widjaja@haw-hamburg.de>
2026-06-29 16:16:32 +0200

    health monitor starts the battery voltage monitoring

--- nodes/firmware/custom-modules/health_monitor/health_monitor.c
+++ nodes/firmware/custom-modules/health_monitor/health_monitor.c
@@ -1,12 +1,14 @@
 #include "health_monitor.h"
 
-health_monitor_t* health_monitor_new(int update_period_sec) {
+health_monitor_t* health_monitor_new(void) {
 	health_monitor_t* monitor = (health_monitor_t*)malloc(sizeof(health_monitor_t));
-	monitor->update_period_sec = update_period_sec;
+	monitor->update_period_sec = 10; // default update period
+	monitor->low_battery_threshold_mv = 3700; // default low battery threshold
+	monitor->is_low_battery = false;
+	monitor->battery_update_period_sec = 10; // default battery update period
 
 	//init battery monitoring
-	//int threshold_mv = 3000; // TODO adjust this later
-	//monitor->battery_monitor = battery_voltage_monitor_new(threshold_mv);
+	monitor->battery_monitor = battery_voltage_monitor_new();
 
 	//init shock detection
 	// int shock_threshold = 15000; // TODO adjust this later
@@ -16,32 +18,55 @@ health_monitor_t* health_monitor_new(int update_period_sec) {
 	return monitor;
 }
 
-static void* thread_function(void* monitor_void) {
-	health_monitor_t* monitor = (health_monitor_t*)monitor_void;
-	while (monitor->running) {
-		// 	//init the payload
-		// 	health_monitor_payload_t payload;
+static void serialize_and_send(const health_monitor_payload_t* payload) {
+	//serialize the payload
+	uint8_t buffer[HEALTH_MONITOR_BUFFER_SIZE];
+	size_t buff_size = sizeof(buffer);
+	health_monitor_serialize_record_no_sig(payload, buffer, &buff_size);
+	LOG_DEBUG("[health_monitor.c:%d] Serialized health monitor payload, size: %d bytes\n", __LINE__, buff_size);
+	for (size_t i = 0; i < buff_size; i++) {
+		LOG_DEBUG("0x%02X\n", buffer[i]);
+	}
+	//send the payload via lorawan
+	int status = send_lorawan_packet(buffer, buff_size);
+	if (status == 0) {
+		LOG_DEBUG("[health_monitor.c:%d] Sent health monitor payload via LoRaWAN\n", __LINE__);
+	} else {
+		LOG_DEBUG("[health_monitor.c:%d] Failed to send health monitor payload via LoRaWAN: lorawan is not initialized yet\n", __LINE__);
+	}
+}
 
-		// 	//TODO get battery info
-		// 	//battery_voltage_monitor_fetch_info(monitor->battery_monitor, &payload.battery_info);
+static void* battery_function(void* instance_void) {
+	health_monitor_t* instance = (health_monitor_t*)instance_void;
+	while (instance->running) {
+		//init the payload
+		health_monitor_payload_t payload;
 
-		// 	//TODO get shock status
-		// 	//shock_detector_fetch_status(monitor->shock_detector, &payload.shock_status);
+		//TODO get battery info
+		battery_info_t battery_info = battery_voltage_monitor_fetch_info(instance->battery_monitor);
+		switch (battery_info.battery_status) {
+			case CHARGING:
+				payload.header = BATTERY_CHARGING;
+				break;
+			case DISCHARGING:
+				payload.header = BATTERY_DISCHARGING;
+				break;
+			default:
+				payload.header = BATTERY_DISCHARGING; // default to discharging if unknown
+				break;
+		}
+		//report the battery state
+		serialize_and_send(&payload);
 
-		// 	//serialize the payload
-		// 	uint8_t buffer[HEALTH_MONITOR_BUFFER_SIZE];
-		// 	size_t buff_size = sizeof(buffer);
-		// 	health_monitor_serialize_record_no_sig(&payload, buffer, &buff_size);
-		// 	LOG_DEBUG("[health_monitor.c:%d] Serialized health monitor payload, size: %d bytes\n", __LINE__, buff_size);
-
-		// 	//send the payload via lorawan
-		// 	int status = send_lorawan_packet(buffer, buff_size);
-		// 	if (status == 0) {
-		// 		LOG_DEBUG("[health_monitor.c:%d] Sent health monitor payload via LoRaWAN\n", __LINE__);
-		// 	} else {
-		// 		LOG_DEBUG("[health_monitor.c:%d] Failed to send health monitor payload via LoRaWAN: lorawan is not initialized yet\n", __LINE__);
-		// 	}
-		// 	ztimer_sleep(ZTIMER_SEC, monitor->update_period_sec);
+		if (battery_info.voltage_mv < instance->low_battery_threshold_mv && !instance->is_low_battery) {
+			instance->is_low_battery = true;
+			//report low battery even
+			payload.header = BATTERY_LOW;
+			serialize_and_send(&payload);
+		} else if (battery_info.voltage_mv >= instance->low_battery_threshold_mv && instance->is_low_battery) {
+			instance->is_low_battery = false;
+		}
+		ztimer_sleep(ZTIMER_SEC, instance->battery_update_period_sec);
 	}
 	return NULL;
 }
@@ -57,12 +82,12 @@ int health_monitor_start(health_monitor_t* monitor) {
 		return -1;
 	}
 
-	monitor->thread_pid = thread_create(monitor->thread_stack,
-										sizeof(monitor->thread_stack),
+	monitor->battery_thread_pid = thread_create(monitor->battery_thread_stack,
+										sizeof(monitor->battery_thread_stack),
 										THREAD_PRIORITY_MAIN - 1,
 										THREAD_CREATE_STACKTEST,
-										thread_function,
-										(void*)monitor, "Health Monitor Thread");
+										battery_function,
+										(void*)monitor, "Battery Thread");
 
 	return 0;
 }
