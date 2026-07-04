8e681cd522b4399535dd6908e479458fa1d86ad7
Maverick <maverick.widjaja@haw-hamburg.de>
2026-07-04 20:14:12 +0200

    refactor new -> init

--- nodes/firmware/custom-modules/health_monitor/battery_voltage_monitor.c
+++ nodes/firmware/custom-modules/health_monitor/battery_voltage_monitor.c
@@ -43,22 +43,22 @@ static int get_battery_voltage(void) {
 	return (int)voltage_mv;
 }
 
-battery_voltage_monitor_t* battery_voltage_monitor_new(void) {
+int battery_voltage_monitor_init(battery_voltage_monitor_t* instance) {
 	gpio_t adc_pin = GPIO_PIN(0, 14);
 	int res = voltage_adc_setup(adc_pin, AIN7_BAT);
 	if (res != 0) {
 		LOG_DEBUG("[battery_voltage_monitor.c:%d] Error setting up voltage adc\n", __LINE__);
-		return NULL;
+		return -1;
 	}
-	battery_voltage_monitor_t* instance = (battery_voltage_monitor_t*)malloc(sizeof(battery_voltage_monitor_t));
+	instance = (battery_voltage_monitor_t*)malloc(sizeof(battery_voltage_monitor_t));
 	if (instance == NULL) {
 		LOG_DEBUG("[battery_voltage_monitor.c:%d] Failed to allocate memory for battery voltage monitor", __LINE__);
-		return NULL;
+		return -1;
 	}
 	//monitor->threshold_mv = threshold_mv;
 	instance->prev_voltage_mv = -1;
 
-	return instance;
+	return 0;
 }
 
 static voltage_trend analyze_voltage_trend(const int prev_voltage_mv, const int current_voltage_mv) {
--- nodes/firmware/custom-modules/health_monitor/include/battery_voltage_monitor.h
+++ nodes/firmware/custom-modules/health_monitor/include/battery_voltage_monitor.h
@@ -75,9 +75,9 @@ typedef struct {
 
 /**
  * @brief Creates a new battery voltage monitor to the heap
- * @return Pointer to the new battery voltage monitor, or NULL if memory allocation failed
+ * @return 0 on success, -1 if memory allocation failed
  */
-battery_voltage_monitor_t* battery_voltage_monitor_new(void);
+int battery_voltage_monitor_init(battery_voltage_monitor_t* monitor);
 
 battery_info_t battery_voltage_monitor_fetch_info(battery_voltage_monitor_t* monitor);
 
