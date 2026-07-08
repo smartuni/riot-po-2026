218b2f64ec177489d3058998f50de7c1c1a3bf18
Maverick <maverick.widjaja@haw-hamburg.de>
2026-07-09 00:23:53 +0200

    add status on serialize_and_send()

--- nodes/firmware/custom-modules/health_monitor/health_monitor.c
+++ nodes/firmware/custom-modules/health_monitor/health_monitor.c
@@ -22,23 +22,19 @@ int health_monitor_init(health_monitor_t* instance) {
 	return 0;
 }
 
-static void serialize_and_send(const health_monitor_payload_t* payload) {
+static int serialize_and_send(const health_monitor_payload_t* payload) {
 	//serialize the payload
 	uint8_t buffer[HEALTH_MONITOR_CBOR_SIZE_BYTES];
 	size_t buff_size = sizeof(buffer);
 	health_monitor_serialize_record_no_sig(payload, buffer, &buff_size);
-	LOG_DEBUG("[health_monitor.c:%d] Serialized health monitor payload, size: %d bytes\n", __LINE__, buff_size);
-	for (size_t i = 0; i < buff_size; i++) {
-		LOG_DEBUG("0x%02X ", buffer[i]);
-	}
-	LOG_DEBUG("\n");
+	// LOG_DEBUG("[health_monitor.c:%d] Serialized health monitor payload, size: %d bytes\n", __LINE__, buff_size);
+	// for (size_t i = 0; i < buff_size; i++) {
+	// 	LOG_DEBUG("0x%02X ", buffer[i]);
+	// }
+	// LOG_DEBUG("\n");
 	//send the payload via lorawan
 	int status = send_lorawan_packet(buffer, buff_size);
-	if (status == 0) {
-		LOG_DEBUG("[health_monitor.c:%d] Sent health monitor payload via LoRaWAN\n", __LINE__);
-	} else {
-		LOG_DEBUG("[health_monitor.c:%d] Failed to send health monitor payload via LoRaWAN: lorawan is not initialized yet\n", __LINE__);
-	}
+	return status;
 }
 
 static void* battery_function(void* instance_void) {
@@ -66,14 +62,18 @@ static void* battery_function(void* instance_void) {
 		payload.body = battery_info.voltage_mv;
 
 		//report the battery state
-		serialize_and_send(&payload);
+		if(serialize_and_send(&payload) != 0) {
+			LOG_ERROR("[health_monitor.c:%d] Failed to send battery status payload\n", __LINE__);
+		}
 
 		// check for low battery and report if it's not yet reported
 		if (battery_info.voltage_mv < low_battery_threshold_mv && !is_low_battery) {
 			//report low battery one time and set the flag
 			payload.header = BATTERY_LOW;
 			payload.body = battery_info.voltage_mv;
-			serialize_and_send(&payload);
+			if(serialize_and_send(&payload) != 0) {
+				LOG_ERROR("[health_monitor.c:%d] Failed to send low battery payload\n", __LINE__);
+			}
 			is_low_battery = true;
 
 			//reset the low battery flag after the battery voltage goes above the threshold
@@ -97,7 +97,9 @@ static void* shock_detector_function(void* instance_void) {
 		payload.header = SHOCK_STATUS;
 		payload.body = SHOCK_DETECTED;
 		LOG_DEBUG("[health_monitor.c:%d] Shock detected, sending payload...\n", __LINE__);
-		serialize_and_send(&payload);
+		if(serialize_and_send(&payload) != 0) {
+			LOG_ERROR("[health_monitor.c:%d] Failed to send shock status payload\n", __LINE__);
+		}
 	}
 	return NULL;
 }
@@ -113,14 +115,6 @@ int health_monitor_start(health_monitor_t* instance) {
 	// 	return -1;
 	// }
 
-	LOG_DEBUG("[health_monitor.c:%d] Starting the battery monitoring thread...\n", __LINE__);
-	instance->battery_monitor_running = true;
-	instance->battery_thread_pid = thread_create(instance->battery_thread_stack,
-												 sizeof(instance->battery_thread_stack),
-												 THREAD_PRIORITY_MAIN - 1,
-												 THREAD_CREATE_STACKTEST,
-												 battery_function,
-												 (void*) &instance->battery_instance, "Battery Thread");
 	instance->shock_detector_running = true;
 	instance->shock_detector_thread_pid = thread_create(instance->shock_detector_thread_stack,
 														sizeof(instance->shock_detector_thread_stack),
@@ -128,6 +122,15 @@ int health_monitor_start(health_monitor_t* instance) {
 														THREAD_CREATE_STACKTEST,
 														shock_detector_function,
 														(void*) &instance->shock_detector_instance, "Shock Detector Thread");
+
+	instance->battery_monitor_running = true;
+	instance->battery_thread_pid = thread_create(instance->battery_thread_stack,
+												 sizeof(instance->battery_thread_stack),
+												 THREAD_PRIORITY_MAIN - 1,
+												 THREAD_CREATE_STACKTEST,
+												 battery_function,
+												 (void*) &instance->battery_instance, "Battery Thread");
+	
 
 	return 0;
 }
