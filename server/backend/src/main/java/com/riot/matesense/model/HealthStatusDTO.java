0d0cc80fd84ada6e427ff796c27c7321c892b858
theunlimited64 <theunlimited64@gmail.com>
2026-07-09 14:35:28 +0200

    refactor(backend): update HealthStatusDTO for FreeFallStatus

--- server/backend/src/main/java/com/riot/matesense/model/HealthStatusDTO.java
+++ server/backend/src/main/java/com/riot/matesense/model/HealthStatusDTO.java
@@ -1,12 +1,12 @@
 package com.riot.matesense.model;
 
 import com.riot.matesense.enums.BatteryStatus;
-import com.riot.matesense.enums.ShockStatus;
+import com.riot.matesense.enums.FreeFallStatus;
 
 public class HealthStatusDTO {
     private int version;
     private int senseGateId;
-    private ShockStatus shockStatus;
+    private FreeFallStatus freeFallStatus;
     private BatteryStatus batteryStatus;
     private int voltageMv;
 
@@ -15,10 +15,10 @@ public class HealthStatusDTO {
     }
 
     // Komfort-Konstruktor für den Formatter
-    public HealthStatusDTO(int version, int senseGateId, ShockStatus shockStatus, BatteryStatus batteryStatus, int voltageMv) {
+    public HealthStatusDTO(int version, int senseGateId, FreeFallStatus freeFallStatus, BatteryStatus batteryStatus, int voltageMv) {
         this.version = version;
         this.senseGateId = senseGateId;
-        this.shockStatus = shockStatus;
+        this.freeFallStatus = freeFallStatus;
         this.batteryStatus = batteryStatus;
         this.voltageMv = voltageMv;
     }
@@ -30,8 +30,8 @@ public class HealthStatusDTO {
     public int getSenseGateId() { return senseGateId; }
     public void setSenseGateId(int senseGateId) { this.senseGateId = senseGateId; }
 
-    public ShockStatus getShockStatus() { return shockStatus; }
-    public void setShockStatus(ShockStatus shockStatus) { this.shockStatus = shockStatus; }
+    public FreeFallStatus getFreeFallStatus() { return freeFallStatus; }
+    public void setFreeFallStatus(FreeFallStatus freeFallStatus) { this.freeFallStatus = freeFallStatus; }
 
     public BatteryStatus getBatteryStatus() { return batteryStatus; }
     public void setBatteryStatus(BatteryStatus batteryStatus) { this.batteryStatus = batteryStatus; }
