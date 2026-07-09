71ca84e4d810d6a22028b15e3408a5557715e441
Maverick <maverick.widjaja@haw-hamburg.de>
2026-07-09 18:49:19 +0200

    Health monitoring

---
+++ nodes/firmware/custom-modules/cbor_serialization/health_monitor_serialization.c
@@ -0,0 +1,20 @@
+#include "health_monitor_serialization.h"
+
+
+int health_monitor_serialize(const health_monitor_payload_t* record, uint8_t* buf, size_t* out_len) {
+    CborEncoder root_encoder;
+	cbor_encoder_init(&root_encoder, buf, *out_len, 0);
+	CborEncoder main_array_encoder;
+	cbor_encoder_create_array(&root_encoder, &main_array_encoder, HEALTH_MONITOR_CBOR_SIZE_COUNT); // +3 for version, msg_type, and node_id
+
+    cbor_encode_uint(&main_array_encoder, ENCODING_V_1);
+    cbor_encode_uint(&main_array_encoder, MESSAGE_TYPE_HEALTH_MONITOR);
+    uint32_t node_id = (uint32_t)self_node_id;
+    cbor_encode_uint(&main_array_encoder, node_id);
+	cbor_encode_uint(&main_array_encoder, record->header);
+    cbor_encode_uint(&main_array_encoder, record->body);
+	cbor_encoder_close_container(&root_encoder, &main_array_encoder);
+
+    *out_len = cbor_encoder_get_buffer_size(&root_encoder, buf);
+    return 0;
+}
