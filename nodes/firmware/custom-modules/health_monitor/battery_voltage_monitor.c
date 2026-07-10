617ab40d1bc69c282f4c2bde8a0a433d3011b795
Maverick <maverick.widjaja@haw-hamburg.de>
2026-07-10 10:39:14 +0200

    pass the value to be retrieved via an out-parameter

--- nodes/firmware/custom-modules/health_monitor/battery_voltage_monitor.c
+++ nodes/firmware/custom-modules/health_monitor/battery_voltage_monitor.c
@@ -65,38 +65,38 @@ static voltage_trend analyze_voltage_trend(const int prev_voltage_mv, const int 
 	return VOLTAGE_TREND_UNKNOWN;
 }
 
-battery_info_t battery_voltage_monitor_fetch_info(battery_voltage_monitor_t* instance) {
-	battery_info_t info;
-	info.voltage_mv = get_battery_voltage();
+void battery_voltage_monitor_fetch_info(battery_voltage_monitor_t* instance, battery_info_t* info) {
+	assert(instance != NULL);
+	assert(info != NULL);
+	info->voltage_mv = get_battery_voltage();
 	if (instance->prev_voltage_mv == -1) {
-		instance->prev_voltage_mv = info.voltage_mv;
+		instance->prev_voltage_mv = info->voltage_mv;
 	}
-	LOG_DEBUG("[battery_voltage_monitor.c:%d] Current battery voltage: %d mV\n", __LINE__, info.voltage_mv);
-	voltage_trend trend = analyze_voltage_trend(instance->prev_voltage_mv, info.voltage_mv);
+	LOG_DEBUG("[battery_voltage_monitor.c:%d] Current battery voltage: %d mV\n", __LINE__, info->voltage_mv);
+	voltage_trend trend = analyze_voltage_trend(instance->prev_voltage_mv, info->voltage_mv);
 	switch (trend) {
 		case VOLTAGE_TREND_INCREASING: {
 			LOG_DEBUG("[battery_voltage_monitor.c:%d] Battery voltage is increasing\n", __LINE__);
-			info.battery_status = BATTERY_STATE_CHARGING;
+			info->battery_status = BATTERY_STATE_CHARGING;
 			break;
 		}
 		case VOLTAGE_TREND_DECREASING: {
 			LOG_DEBUG("[battery_voltage_monitor.c:%d] Battery voltage is decreasing\n", __LINE__);
-			info.battery_status = BATTERY_STATE_DISCHARGING;
+			info->battery_status = BATTERY_STATE_DISCHARGING;
 			break;
 		}
 		case VOLTAGE_TREND_STABLE: {
 			LOG_DEBUG("[battery_voltage_monitor.c:%d] Battery voltage is stable\n", __LINE__);
-			info.battery_status = instance->last_battery_status;
+			info->battery_status = instance->last_battery_status;
 			break;
 		}
 		default: {
 			LOG_DEBUG("[battery_voltage_monitor.c:%d] Battery status is unknown\n", __LINE__);
-			info.battery_status = BATTERY_STATE_UNKNOWN;
+			info->battery_status = BATTERY_STATE_UNKNOWN;
 			break;
 		}
 	}
-	instance->last_battery_status = info.battery_status;
-	instance->prev_voltage_mv = info.voltage_mv;
-	return info;
+	instance->last_battery_status = info->battery_status;
+	instance->prev_voltage_mv = info->voltage_mv;
 }
 
