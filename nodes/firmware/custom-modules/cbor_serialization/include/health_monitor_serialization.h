f8c75db03cf3d91547d8baae53b4f662180770d7
Maverick <83001397+Arclythe@users.noreply.github.com>
2026-07-12 15:49:12 +0200

    [NEW] Health monitoring integration stage (#121)
    * Health monitoring
    
    * removed some commented blocks
    
    * added error handling
    
    * removed #pragma, adjusted LOG_DEBUG
    
    * refined the code comments
    
    * - added error handling;
    - removed unused running flag
    
    * removed accidental duplicate includes
    
    * refactor: replace hardcoded values with defined constants
    
    * refactor to thread_XXX
    
    * add health monitor error handling in main
    
    * removed delete functions
    
    * removed the comment
    
    * reverted to  LOG_INFO due to compiler error
    
    * pass the value to be retrieved via an out-parameter
    
    * removed health monitor start and moved the logic to init
    
    * fix(nodes/cbor): include dependencies in Makefile

---
+++ nodes/firmware/custom-modules/cbor_serialization/include/health_monitor_serialization.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2025 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @brief       Health Monitor Serialization for senseGate
+ * @author      Maverick Widjaja <Maverick.widjaja@haw-hamburg.de>
+ */
+
+#ifndef HEALTH_MONITOR_SERIALIZATION_H
+#define HEALTH_MONITOR_SERIALIZATION_H
+
+#include "health_monitor_payload.h"
+#include "cbor_serialization.h"
+#include "personalization.h"
+#include "cbor.h"
+#include <stdint.h>
+
+#define MESSAGE_TYPE_HEALTH_MONITOR 0x05
+#define HEALTH_MONITOR_CBOR_SIZE_COUNT (HEALTH_MONITOR_PAYLOAD_ELEMENTS + 3) // +3 for version, msg_type, and node_id
+#define HEALTH_MONITOR_CBOR_SIZE_BYTES 9 //see health_monitor_serialization.c
+
+/**
+ * @brief Same as cbor_serialize_record but leave out signature field
+ *
+ * @param record    Pointer to the record to serialize
+ * @param out       Pointer to the buffer that holds enough bytes to store the record.
+ * @param out_len   Pointer to the size of @p out, it will return with the amount of used
+ *                  bytes.
+ *
+ * @retval 0 on success
+ * @retval negative value on error
+ */
+int health_monitor_serialize(const health_monitor_payload_t* record, uint8_t* out, size_t* out_len);
+
+#endif
